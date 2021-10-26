49f14b6c5d1a126da44bfac967099351237593a2
Yaroslav Piskunov <kinglordlear@gmail.com>
2021-10-26 15:02:33 +0300

    somewhat filter

--- src/tc1/src/scanfilter.py
+++ src/tc1/src/scanfilter.py
@@ -2,6 +2,7 @@
 
 import rospy
 from sensor_msgs.msg import LaserScan
+from math import fabs
 
 
 class ScanFilter:
@@ -11,6 +12,16 @@ class ScanFilter:
 		self.pub = rospy.Publisher('/base_scan_filtered', LaserScan, queue_size=1)
 
 	def filter(self, msg):
+		ranges = list(msg.ranges)
+		filtered_ranges = []
+		mean = 0
+		for i in range(3, len(ranges)):
+			mean = (ranges[i-3] + ranges[i-2] + ranges[i-1])/3
+			if not fabs(ranges[i] - mean) > 0.2 and ranges[i] < msg.range_max and ranges[i] > msg.range_min:
+				filtered_ranges.append(ranges[i])
+				ranges[i] = mean
+			
+		msg.ranges = tuple(filtered_ranges)
 		self.pub.publish(msg)
 
 
